c7bdd93c91e670122f16aad3766876046b5f8be2
Isaiah-Young <69137368+Isaiah-Young@users.noreply.github.com>
2020-09-03 23:44:31 -0500

    js available on all pages now

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -22,18 +22,7 @@ namespace Portfolio.Controllers
 
         public IActionResult Index()
         {
-            List<ArtGallery> Gallery = new List<ArtGallery>()
-            {
-                new ArtGallery() {Name = "Tiger", Directory = "../Style/Images/Tiger.jpg"},
-                new ArtGallery() {Name = "Jaguar", Directory = "../Style/Images/Jaguar.jpg"},
-                new ArtGallery() {Name = "Skull", Directory = "../Style/Images/Skull.jpg"},
-                new ArtGallery() {Name = "Davy", Directory = "../Style/Images/Davy.jpg"},
-                new ArtGallery() {Name = "Wren", Directory = "../Style/Images/Wren.jpg"},
-                new ArtGallery() {Name = "Mackys", Directory = "../Style/Images/Mackys.jpg"},
-                new ArtGallery() {Name = "Henry", Directory = "../Style/Images/Henry.jpg"}
-            };
-
-            return View(Gallery);
+            return View();
         }
 
         public IActionResult Privacy()
